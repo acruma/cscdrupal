2096cf63c185a5ac0a05cf35f03b3340538dda51
Mateo <macebedo@pulsia.es>
2020-01-28 20:54:02 +0100

    subir

--- js/pulsiaCustom.js
+++ js/pulsiaCustom.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
 		
 	$("div#block-views-block-boton-burguer-svg-block-1").click(function () {
-		$(".ps-sidebar-grid").toggleClass("ps-showsidebar", 1000);
+		$(".ps-sidebar-grid").toggleClass("ps-showsidebar", 5000);
 	});
 	
 	$("div#block-views-block-boton-cerrar-o-cancel-svg-block-1").click(function () {
-		$(".ps-sidebar-grid").toggleClass("ps-showsidebar", 1000);
+		$(".ps-sidebar-grid").toggleClass("ps-showsidebar", 5000);
 	});
 	
 });
