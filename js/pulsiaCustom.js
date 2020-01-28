fc0e042134200c37967c33462b8061bcb353381e
Mateo <macebedo@pulsia.es>
2020-01-28 18:27:19 +0100

    subir

--- js/pulsiaCustom.js
+++ js/pulsiaCustom.js
@@ -1,22 +1,11 @@
 jQuery(document).ready(function($){
 		
-	var maxHumedad = $('.ps-optimo-humedad-max').text();
-	var maxHumedad = $.trim(maxHumedad);
-	var maxHumedad = parseInt(maxHumedad);
+	$("div#block-views-block-boton-burguer-svg-block-1").click(function () {
+		$(".ps-sidebar-grid").toggleClass("ps-showsidebar");
+	});
 	
-	var minHumedad = $('.ps-optimo-humedad-min').text();
-	var minHumedad = $.trim(minHumedad);
-	var minHumedad = parseInt(minHumedad);
-	
-	var realHumedad = $('.ps-valor-humedad').text();
-	var realHumedad = $.trim(realHumedad);
-	var realHumedad = parseInt(realHumedad);
-	
-	
-	if(realHumedad >= minHumedad && realHumedad <= maxHumedad){
-		$('.ps-valor-humedad').after( "Correcto" );
-	}else{
-		$('.ps-valor-humedad').after( "Incorrecto" );
-	}
+	$("div#block-views-block-boton-cerrar-o-cancel-svg-block-1").click(function () {
+		$(".ps-sidebar-grid").toggleClass("ps-showsidebar");
+	});
 	
 });
