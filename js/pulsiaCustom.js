ecf66e28b6816b9fb16573fab1ff585d1b8aea09
Mateo <macebedo@pulsia.es>
2020-05-11 22:18:58 +0200

    cambios

--- js/pulsiaCustom.js
+++ js/pulsiaCustom.js
@@ -11,9 +11,9 @@ jQuery(document).ready(function($){
 	/*OBETENEMOS EL VALOR DEL INPUT Y EJECUTAMOS EL BOTON VER MIS TARIFAS*/
 
 	$("div#block-views-block-tarifas-y-promociones-a-tu-medida-block-2-block-1").find(".view-content .views-row:nth-child(4)").click(function () {
-		var codigopostal = input[name='cp'];
+		var codigopostal = $("input[name='cp']");
 		var urlopen = 'tarifas-por-poblacion?cp=' + codigopostal;
-		window.open(urlopen, '_self');
+		//window.open(urlopen, '_self');
 		console.log(codigopostal);
 	});
 	// 
