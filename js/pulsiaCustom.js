c938330dd66734eb628835910c049c107e04b98f
Mateo <macebedo@pulsia.es>
2020-05-12 16:00:45 +0200

    cambios

--- js/pulsiaCustom.js
+++ js/pulsiaCustom.js
@@ -26,7 +26,31 @@ jQuery(document).ready(function($){
 			window.open(urlopen, '_self');
 			console.log(codigopostal);
 		}else{
-			//Pintar un label para decir que se requiere un cp?
+			//Pintar un label para decir que se requiere un cp? #todo
 		}
 	}
+
+	/* OBTENGO EL DATO CP DE URL Y LO SE PASO A LA CLASE .csc-cp-tar */
+	/*FUNCION GET PARAMETROS URL*/
+	var getUrlParameter = function getUrlParameter(sParam) {
+		var sPageURL = window.location.search.substring(1),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+	
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+	
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+			}
+		}
+	};
+
+	if (window.location.href.indexOf("tarifas-por-poblacion") > -1) {
+		var cp_tar = getUrlParameter('cp');
+		$("div#block-views-block-promociones-radio-block-1").find(".csc-cp-tar").text(cp_tar)
+	}
+
+
 });
