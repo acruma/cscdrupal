99a506300778eff2446582b4c17ee07fb4c311e0
Mateo <macebedo@pulsia.es>
2020-05-13 16:43:16 +0200

    cambios

--- js/pulsiaCustom.js
+++ js/pulsiaCustom.js
@@ -55,8 +55,7 @@ jQuery(document).ready(function($){
 
 
 	/*Mandar por URL el valor dataTid obtenido del form */
-
-	$('div#block-views-block-promociones-radio-block-1').on('click', '.csc-contratar', function () {
+	$(".csc-contratar").click(function () {
 		$(this).closest("div#block-views-block-promociones-radio-block-1").find(".csc-itemsExtras").each(function( index ) {
 			//$("input[dataTid]").val();
 			console.log( index + ": " + $( this ).text() );
