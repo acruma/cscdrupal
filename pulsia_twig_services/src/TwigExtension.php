c05d01efe595db4347066bc6bc00b8c40d79e387
Mateo <macebedo@pulsia.es>
2020-05-13 13:24:22 +0200

    cambios

---
+++ pulsia_twig_services/src/TwigExtension.php
@@ -0,0 +1,41 @@
+<?php
+namespace Drupal\pulsia_twig_services;
+use Drupal\block\Entity\Block;
+use Drupal\user\Entity\User;
+use Drupal\node\Entity\Node;
+ 
+/**
+ * Class DefaultService.
+ *
+ * @package Drupal\demo_module
+ */
+class TwigExtension extends \Twig_Extension {
+ 
+  /**
+   * {@inheritdoc}
+   * This function must return the name of the extension. It must be unique.
+   */
+  public function getName() {
+    return 'block_display';
+  }
+ 
+  /**
+   * In this function we can declare the extension function.
+   */
+  public function getFunctions() {
+    return array(
+      new \Twig_SimpleFunction('getAliasUrl', array($this, 'getAliasUrl'), array('is_safe' => array('html'))),
+      new \Twig_SimpleFunction('getPrueba', array($this, 'getPrueba'), array('is_safe' => array('html'))),
+    );
+  }
+  
+  public function getAliasUrl($nid) {
+    $alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/'.$nid);
+    return $alias;
+  }
+
+  public function getPrueba() {
+    $alias = "drupal 8";
+    return $alias;
+  }
+}
